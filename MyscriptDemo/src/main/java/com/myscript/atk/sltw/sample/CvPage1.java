70318c1a8dd11b1a24812e3922f50484d8880d7f
Ziyao LI <Ziyao LI>
2016-11-29 18:29:01 +0100

    mysript demo

---
+++ MyscriptDemo/src/main/java/com/myscript/atk/sltw/sample/CvPage1.java
@@ -0,0 +1,15 @@
+package com.myscript.atk.sltw.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+public class CvPage1 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edu);
+    }
+}
---
+++ MyscriptDemo/src/main/java/com/myscript/atk/sltw/sample/CvPage3.java
@@ -0,0 +1,15 @@
+package com.myscript.atk.sltw.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+public class CvPage3 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exp);
+    }
+}
